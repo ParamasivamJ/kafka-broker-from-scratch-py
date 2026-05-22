bae1b492c061a8d745758c9ed488612d6d4a8b19
ParamasivamJ <paramasivamp886@gmail.com>
2026-05-22 16:25:56 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,18 +10,20 @@ def main():
 
     print(f"Connection from {addr}")
 
-    # Receive request (not used yet)
-    conn.recv(1024)
+    # Receive request
+    request = conn.recv(1024)
+
+    # Extract correlation_id (bytes 8-11)
+    correlation_id = request[8:12]
 
     # Build response
-
     message_size = (0).to_bytes(4, byteorder="big")
-    correlation_id = (7).to_bytes(4, byteorder="big")
 
     response = message_size + correlation_id
 
     # Send response
     conn.sendall(response)
+
 
 
 if __name__ == "__main__":
