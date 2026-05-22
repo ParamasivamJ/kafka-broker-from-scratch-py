a19f95ba670fc46a5faef9669244bbe5369eb461
ParamasivamJ <paramasivamp886@gmail.com>
2026-05-22 16:46:50 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import socket  # noqa: F401
 
-
 def main():
     
     print("Logs from your program will appear here!")
@@ -13,13 +12,33 @@ def main():
     # Receive request
     request = conn.recv(1024)
 
-    # Extract correlation_id (bytes 8-11)
+    # Extract fields
+    api_version = int.from_bytes(
+        request[6:8],
+        byteorder="big"
+    )
     correlation_id = request[8:12]
 
+    # Validate API version
+    if 0 <= api_version <= 4:
+        error_code = 0
+    else:
+        error_code = 35
+
+    
     # Build response
     message_size = (0).to_bytes(4, byteorder="big")
 
-    response = message_size + correlation_id
+    error_code_bytes = error_code.to_bytes(
+        2,
+        byteorder="big"
+    )
+
+    response = (
+        message_size +
+        correlation_id +
+        error_code_bytes
+    )
 
     # Send response
     conn.sendall(response)
