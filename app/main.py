efe3ab550e28f00ac01c8abac9958ccbba969d9a
ParamasivamJ <paramasivamp886@gmail.com>
2026-05-22 16:14:15 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,22 @@ def main():
     print("Logs from your program will appear here!")
 
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    conn, addr = server.accept()
+
+    print(f"Connection from {addr}")
+
+    # Receive request (not used yet)
+    conn.recv(1024)
+
+    # Build response
+
+    message_size = (0).to_bytes(4, byteorder="big")
+    correlation_id = (7).to_bytes(4, byteorder="big")
+
+    response = message_size + correlation_id
+
+    # Send response
+    conn.sendall(response)
 
 
 if __name__ == "__main__":
